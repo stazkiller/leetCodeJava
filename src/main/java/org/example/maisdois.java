8144a8caafaa2f004b93944b4e8dba2b25401f15
staz <bryan.hawk022@gmail.com>
2025-11-25 12:23:27 -0300

    Add files via upload
    estruturas

---
+++ src/main/java/org/example/maisdois.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class maisdois {
+
+    //26. Remove Duplicates from Sorted Array
+    public int removeDuplicates(int[] nums) {
+        if (nums.length == 0) return 0;
+
+        int slow = 0;
+        for (int fast = 1; fast < nums.length; fast++) {
+            if (nums[fast] != nums[slow]) {
+                slow++;
+                nums[slow] = nums[fast];
+            }
+        }
+
+        return slow + 1;
+    }
+}
