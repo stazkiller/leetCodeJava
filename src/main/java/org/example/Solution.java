610c75309ff6dee70cd57c85835bae13423dd5a3
staz <bryan.hawk022@gmail.com>
2025-11-26 13:18:24 -0300

    Add files via upload

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -5,7 +5,6 @@ public class Solution {
     //26. Remove Duplicates from Sorted Array
     public int removeDuplicates(int[] nums) {
         if (nums.length == 0) return 0;
-
         int slow = 0;
         for (int fast = 1; fast < nums.length; fast++) {
             if (nums[fast] != nums[slow]) {
@@ -13,7 +12,6 @@ public class Solution {
                 nums[slow] = nums[fast];
             }
         }
-
         return slow + 1;
     }
 }
--- src/main/java/org/example/exibicao.java
+++ src/main/java/org/example/exibicao.java
@@ -1,9 +1,11 @@
 package org.example;
 
 public class exibicao {
+
     public static void main(String[] args) {
         Solution solution = new Solution();
-
-        System.out.println(solution.removeDuplicates(new int[]{1, 1, 2, 2, 3, 4, 5}));
+        System.out.println(
+            solution.removeDuplicates(new int[] { 1, 1, 2, 2, 3, 4, 5 })
+        );
     }
 }
